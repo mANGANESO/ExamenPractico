1e32e7057b3c417fbafd083b7713702166b69231
Fred <fred.cruz193@tectijuana.edu.mx>
2020-07-03 20:51:20 -0700

    Agregando codigo para anadir nodo

---
+++ Nodo.java
@@ -0,0 +1,18 @@
+public class Nodo{
+	Object valor;
+	Nodo siguiente;
+
+ public Nodo(Object valor){
+   this.valor = valor;
+   this.siguiente = null;
+ }
+ public Object obtenerValor(){
+ 	return valor;
+ }
+ public void enlazarSiguiente(Nodo nodo){
+   siguiente = nodo;
+ }
+ public Nodo obtenerSiguiente(){
+   return siguiente;
+ }
+}
